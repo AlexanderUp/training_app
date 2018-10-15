33ec608d96b3b1b7d6903e461e8d75052b1d36e9
AlexanderUp <sanchezzz90210@yandex.ru>
2018-10-15 23:35:29 +0300

    Alpha version

--- day_calc/dayCalc.py
+++ day_calc/dayCalc.py
@@ -8,14 +8,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-# from kivy.properties import DictProperty
-# from kivy.properties import StringProperty
-# from kivy.properties import ObjectProperty
 
 from kivy.config import Config
 Config.set('graphics', 'height', '288')
 Config.set('graphics', 'width', '512')
-# Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'resizable', '0')
 import sys
 
 print("*" * 125)
@@ -35,40 +32,40 @@ class MainLayout(BoxLayout):
         super(MainLayout, self).__init__(**kwargs)
         self.orientation = 'vertical'
         self.inpl = InputLayout()
+        self.cal = LeapCalendar()
         self.add_widget(self.inpl)
         self.add_widget(Button(text='Calculate', on_press=self.calculate_dates, size_hint=(1, 0.2)))
 
     def calculate_dates(self, *args,  **kwargs):
         print('Args recieved: {}'.format(*args))
         print('Kwargs recieved: {}'.format(kwargs))
-        cal = LeapCalendar()
         dates = [self.inpl.year1.text, self.inpl.month1.text, self.inpl.day1.text, self.inpl.year2.text, self.inpl.month2.text, self.inpl.day2.text]
-        # dates = [int(date) for date in dates]
         try:
-            date = [int(x) for x in date]
+            dates = [int(date) for date in dates]
         except ValueError:
             print('Wrong date inputed!')
+            print('Exiting....')
+            sys.exit()
         else:
             print('Dates inputed: {}'.format(dates))
-            # validation to be here
-        return cal.daysBetweenDates(*dates)
+            if not self.validate(dates[:3]) or not self.validate(dates[3:]):
+                print('Incorrect dates!!')
+                self.inpl.days_label.text = 'Incorrect dates!!'
+                return
+            else:
+                print('Correct input!')
+        self.inpl.days_label.text = str(self.cal.daysBetweenDates(*dates)) + ' days'
 
     def validate(self, date):
         # date is represented as list [year, month, day]
-        cal = LeapCalendar()
-        try:
-            date = [int(x) for x in date]
-        except ValueError:
-            print('Wrong date inputed!')
-        else:
-            if all(d > 0 for d in date):
-                if cal.is_leap(date[0]):
-                    daysOfMonths = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-                else:
-                    daysOfMonths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-                if date[1] > 0 and date[1] <= 12:
-                    if date[2] > 0 and date[2] <= daysOfMonths[date[1]-1]:
-                        return True
+        if all(d > 0 for d in date):
+            if self.cal.is_leap(date[0]):
+                daysOfMonths = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+            else:
+                daysOfMonths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+            if date[1] > 0 and date[1] <= 12:
+                if date[2] > 0 and date[2] <= daysOfMonths[date[1]-1]:
+                    return True
         return False
 
 
@@ -79,7 +76,6 @@ class InputLayout(GridLayout):
         self.cols = 4
         # self.rows = 4
         self.height = 40
-        self.days = 'DAYS'
         self.add_widget(Widget(size_hint=(.25, None)))
         self.add_widget(Label(text='Day', size_hint=(.25, None)))
         self.add_widget(Label(text='Month', size_hint=(.25, None)))
@@ -100,7 +96,8 @@ class InputLayout(GridLayout):
         self.add_widget(self.year2)
         self.add_widget(Widget(size_hint=(.25, None)))
         self.add_widget(Label(text='Total days:', size_hint=(.25, None)))
-        self.add_widget(Label(text=self.days, size_hint=(.25, None)))
+        self.days_label = Label(text='DAYS', size_hint=(0.25, None))
+        self.add_widget(self.days_label)
         self.add_widget(Widget(size_hint=(.25, None)))
 
 
