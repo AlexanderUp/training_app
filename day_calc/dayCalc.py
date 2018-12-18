9cb2b957d58fb984d6e585d02662fa9735bb55c0
AlexanderUp <sanchezzz90210@yandex.ru>
2018-12-18 12:37:58 +0300

    Button "calculate" removed

--- day_calc/dayCalc.py
+++ day_calc/dayCalc.py
@@ -40,52 +40,60 @@ class MainLayout(BoxLayout):
         self.inpl = InputLayout()
         self.cal = LeapCalendar()
         self.add_widget(self.inpl)
-        self.days_label = Label(text='Awaiting input', size_hint=(0.2, None), pos_hint={'center_x':.5})
+        self.days_label = Label(text='Awaiting input...', size_hint=(0.2, None), pos_hint={'center_x':.5})
         self.add_widget(self.days_label)
         self.add_widget(Button(text='Clear', on_press=self.clear, size_hint=(1, 0.2)))
-        self.add_widget(Button(text='Calculate', on_press=self.calculate_dates, size_hint=(1, 0.2)))
+        # self.add_widget(Button(text='Calculate', on_press=self.calculate_dates, size_hint=(1, 0.2)))
 
         self.inpl.today_date1.bind(active=self.on_today_date1_active)
         self.inpl.today_date2.bind(active=self.on_today_date2_active)
         self.inpl.last_day_checkbox.bind(active=self.on_last_day_checkbox_active)
 
+        self.inpl.day1.bind(text=self.calculate_dates)
+        self.inpl.month1.bind(text=self.calculate_dates)
+        self.inpl.year1.bind(text=self.calculate_dates)
+        self.inpl.day2.bind(text=self.calculate_dates)
+        self.inpl.month2.bind(text=self.calculate_dates)
+        self.inpl.year2.bind(text=self.calculate_dates)
 
     def calculate_dates(self, *args,  **kwargs):
-        print('Args recieved: {}'.format(args))
-        print('Kwargs recieved: {}'.format(kwargs))
+        # print('Args recieved: {}'.format(args))
+        # print('Kwargs recieved: {}'.format(kwargs))
         dates = [self.inpl.year1.text, self.inpl.month1.text, self.inpl.day1.text, self.inpl.year2.text, self.inpl.month2.text, self.inpl.day2.text]
-        try:
-            dates = [int(date) for date in dates]
-        except ValueError:
-            print('Wrong data inputed!')
-            self.days_label.text = 'Wrong data inputed!'
-            return None
-        else:
-            print('Dates inputed: {}'.format(dates))
-            if self.validate(dates[:3]) and self.validate(dates[3:]) and self.validate_date_sequence(dates):
-                print('Correct dates!')
-                total_days_between_dates = self.cal.daysBetweenDates(*dates)
-                if self.inpl.last_day_checkbox.active:
-                    total_days_between_dates += 1
-                if total_days_between_dates != 1:
-                    self.days_label.text = str(total_days_between_dates) + ' days total'
-                else:
-                    self.days_label.text = 'Only one day total'
+        if all(dates):
+            try:
+                dates = [int(date) for date in dates]
+            except ValueError:
+                print('Wrong data inputed!')
+                self.days_label.text = 'Wrong data inputed!'
+                return None
             else:
-                print('Incorrect dates!!')
-                self.days_label.text = 'Incorrect dates!!'
+                print('Dates inputed: {}'.format(dates))
+                if self.validate(dates[:3]) and self.validate(dates[3:]) and self.validate_date_sequence(dates):
+                    print('Correct dates!')
+                    total_days_between_dates = self.cal.daysBetweenDates(*dates)
+                    if self.inpl.last_day_checkbox.active:
+                        total_days_between_dates += 1
+                    if total_days_between_dates != 1:
+                        self.days_label.text = str(total_days_between_dates) + ' days total'
+                    else:
+                        self.days_label.text = 'Only one day total'
+                else:
+                    print('Incorrect dates!!')
+                    self.days_label.text = 'Incorrect dates!!'
+        else:
+            self.days_label.text = 'Awaiting input...'
         return None
 
-
     def clear(self, *args):
-        print('Args received: {}'.format(*args))
+        # print('Args received: {}'.format(*args))
         self.inpl.day1.text = ''
         self.inpl.month1.text = ''
         self.inpl.year1.text = ''
         self.inpl.day2.text = ''
         self.inpl.month2.text = ''
         self.inpl.year2.text = ''
-        self.days_label.text = '0 days total'
+        self.days_label.text = 'Awaiting input...'
         self.inpl.today_date1.active = False
         self.inpl.today_date2.active = False
 
@@ -101,13 +109,12 @@ class MainLayout(BoxLayout):
         return False
 
     def validate_date_sequence(self, dates):
-        print('Rcvd for validation: {}'.format(dates))
+        # print('Rcvd for validation: {}'.format(dates))
         year1, month1, day1, year2, month2, day2 = dates
-        print('y1: {}, m1: {}, d1: {}, y2: {}, m2: {}, d2: {}'.format(year1, month1, day1, year2, month2, day2))
+        # print('y1: {}, m1: {}, d1: {}, y2: {}, m2: {}, d2: {}'.format(year1, month1, day1, year2, month2, day2))
         if (year1 < year2) or (year1 == year2 and month1 < month2) or (year1 == year2 and month1 == month2 and day1 <= day2):
             return True
         return False
-
 
     def on_today_date1_active(self, checkbox, value):
         print('Rcvd: {}, value: {}'.format(checkbox, value))
@@ -120,7 +127,7 @@ class MainLayout(BoxLayout):
             self.inpl.day1.text = ''
             self.inpl.month1.text = ''
             self.inpl.year1.text = ''
-            self.days_label.text = 'Awaiting input'
+            self.days_label.text = 'Awaiting input...'
         return None
 
     def on_today_date2_active(self, checkbox, value):
@@ -134,7 +141,7 @@ class MainLayout(BoxLayout):
             self.inpl.day2.text = ''
             self.inpl.month2.text = ''
             self.inpl.year2.text = ''
-            self.days_label.text = 'Awaiting input'
+            self.days_label.text = 'Awaiting input...'
         return None
 
     def on_last_day_checkbox_active(self, checkbox, value, *args, **kwargs):
@@ -148,6 +155,30 @@ class MainLayout(BoxLayout):
                 self.days_label.text = '0 days total'
         return None
 
+    def texting(self, instance, value, *args, **kwargs):
+        print('instance: {}'.format(instance))
+        print('value: {}'.format(value))
+        return None
+
+    # # now can't be implemented because of conflict in realized checkbox logic
+    # def test(self, *args, **kwargs):
+    #     print('Args recieved: {}'.format(args))
+    #     print('Kwargs recieved: {}'.format(kwargs))
+    #     d = datetime.date.today()
+    #     checks_for_checkbox1 = list(zip((self.inpl.day1.text, self.inpl.month1.text, self.inpl.year1.text), (d.day, d.month, d.year)))
+    #     print('checks_for_checkbox1: {}'.format(checks_for_checkbox1))
+    #     for (date_entered, date_today) in checks_for_checkbox1:
+    #         print(date_entered, date_today)
+    #         if self.inpl.today_date1.active and date_entered != str(date_today):
+    #             self.inpl.today_date1.active = False
+    #             print('checkbox1 status changed')
+    #     checks_for_checkbox2 = list(zip((self.inpl.day2.text, self.inpl.month2.text, self.inpl.year2.text), (d.day, d.month, d.year)))
+    #     print('checks_for_checkbox2: {}'.format(list(checks_for_checkbox2)))
+    #     for date_entered, date_today in checks_for_checkbox2:
+    #         if self.inpl.today_date2.active and date_entered != str(date_today):
+    #             self.inpl.today_date2.active = False
+    #             print('checkbox2 status changed')
+    #     return None
 
 
 class InputLayout(GridLayout):
@@ -204,7 +235,7 @@ class DayCalcApp(App):
         return root
 
     def update_rect(self, instance, value):
-        print('update_rect received: {}'.format((instance, value)))
+        # print('update_rect received: {}'.format((instance, value)))
         self.rect.pos = instance.pos
         self.rect.size = instance.size
 
@@ -212,22 +243,22 @@ class DayCalcApp(App):
 if __name__ == '__main__':
     print('Args rcvd: {}'.format(sys.argv))
     DayCalcApp().run()
-    print('*' * 12 + ' Some tests ' + '*' * 12)
-    t = MainLayout()
-    test_cases = [(2012, 2, 28),
-                  (2012, 3, 1),
-                  (2012, 6, 30),
-                  (2012, 8, 8),
-                  (1999, 12, 31),
-                  (2012, 1, 1),
-                  (2012, 1, 1),
-                  (2011, 6, 30),
-                  (2011, 1, 1),
-                  (1900, 1, 1),
-                  (2012, 2, 29),
-                  (2013, 2, 29),
-                  (2018, 10, 45),
-                  (2018, 15, 12),
-                  (2011, -12, 2),]
-    for test in test_cases:
-        print('Tested: {:15} with result {}'.format(str(test), t.validate(test)))
+    # print('*' * 12 + ' Some tests ' + '*' * 12)
+    # t = MainLayout()
+    # test_cases = [(2012, 2, 28),
+    #               (2012, 3, 1),
+    #               (2012, 6, 30),
+    #               (2012, 8, 8),
+    #               (1999, 12, 31),
+    #               (2012, 1, 1),
+    #               (2012, 1, 1),
+    #               (2011, 6, 30),
+    #               (2011, 1, 1),
+    #               (1900, 1, 1),
+    #               (2012, 2, 29),
+    #               (2013, 2, 29),
+    #               (2018, 10, 45),
+    #               (2018, 15, 12),
+    #               (2011, -12, 2),]
+    # for test in test_cases:
+    #     print('Tested: {:15} with result {}'.format(str(test), t.validate(test)))
